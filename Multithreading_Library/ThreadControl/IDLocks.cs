0d62ae2ef02855e58d1a8afa160c9f844abb100e
Julian B <bechti44@googlemail.com>
2022-08-25 15:23:32 +0200

    added very simple class to obtain locks by key, allowing for many, many threads

---
+++ Multithreading_Library/ThreadControl/IDLocks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading_Library.ThreadControl
+{
+    public class IDLocks<T>
+    {
+        ConcurrentDictionary<T,object> locks = new ConcurrentDictionary<T,object>();
+        public object ObtainLockObject(T key)
+        {
+            return locks.GetOrAdd(key, new object());
+        }
+    }
+}
