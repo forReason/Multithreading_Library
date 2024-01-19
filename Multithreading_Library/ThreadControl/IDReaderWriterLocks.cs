65bc85afc3d3fe12b9a213f9870ac0bc8d3c886b
Julian B <bechti44@googlemail.com>
2024-01-19 09:36:38 +0100

    implement IDreaderWriterLock

---
+++ Multithreading_Library/ThreadControl/IDReaderWriterLocks.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Multithreading_Library.ThreadControl
+{
+    /// <summary>
+    /// Represents a collection of ReaderWriterLockSlim objects identified by keys of type <typeparamref name="T"/>.
+    /// This class allows you to create and manage reader-writer locks on a per-key basis, facilitating
+    /// synchronization for read-heavy and write operations in a concurrent environment.
+    /// </summary>
+    /// <typeparam name="T">The type of the keys used to identify ReaderWriterLockSlim objects.</typeparam>
+    public class IDReaderWriterLocks<T>
+    {
+        private ConcurrentDictionary<T, ReaderWriterLockSlim> locks = new ConcurrentDictionary<T, ReaderWriterLockSlim>();
+
+        /// <summary>
+        /// Obtain (get or create) a ReaderWriterLockSlim object associated with a specific id. If a lock object with the specified
+        /// id doesn't already exist, a new one is created. This method is useful for synchronizing access to resources 
+        /// that are read-heavy and occasionally written to, and are identified by keys.
+        /// </summary>
+        /// <param name="key">The key to identify the ReaderWriterLockSlim object.</param>
+        /// <returns>A ReaderWriterLockSlim object that can be used for reader-writer synchronization.</returns>
+        public ReaderWriterLockSlim ObtainLockObject(T key)
+        {
+            return locks.GetOrAdd(key, _ => new ReaderWriterLockSlim());
+        }
+    }
+}
---
+++ Multithreading_Unit_Tests/ThreadControl/IDReaderWriterLocksTests.cs
@@ -0,0 +1,54 @@
+﻿using Multithreading_Library.ThreadControl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Multithreading_Unit_Tests.ThreadControl
+{
+    public class IDReaderWriterLocksTests
+    {
+        [Fact]
+        public void ObtainLockObject_WithKey_ReturnsReaderWriterLockSlim()
+        {
+            // Arrange
+            var idReaderWriterLocks = new IDReaderWriterLocks<string>();
+
+            // Act
+            var lockSlim = idReaderWriterLocks.ObtainLockObject("testKey");
+
+            // Assert
+            Assert.NotNull(lockSlim);
+            Assert.IsType<ReaderWriterLockSlim>(lockSlim);
+        }
+
+        [Fact]
+        public void ObtainLockObject_WithSameKey_ReturnsSameLockInstance()
+        {
+            // Arrange
+            var idReaderWriterLocks = new IDReaderWriterLocks<string>();
+            var firstLock = idReaderWriterLocks.ObtainLockObject("testKey");
+
+            // Act
+            var secondLock = idReaderWriterLocks.ObtainLockObject("testKey");
+
+            // Assert
+            Assert.Same(firstLock, secondLock);
+        }
+
+        [Fact]
+        public void ObtainLockObject_WithDifferentKeys_ReturnsDifferentLockInstances()
+        {
+            // Arrange
+            var idReaderWriterLocks = new IDReaderWriterLocks<string>();
+            var firstLock = idReaderWriterLocks.ObtainLockObject("testKey1");
+            var secondLock = idReaderWriterLocks.ObtainLockObject("testKey2");
+
+            // Act and Assert
+            Assert.NotSame(firstLock, secondLock);
+        }
+    }
+}
