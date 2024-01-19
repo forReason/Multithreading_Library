949be945967f06f2fc54aabe367d229110780be0
Julian B <bechti44@googlemail.com>
2024-01-19 09:13:48 +0100

    - improved documentation - improved performance

--- Multithreading_Library/ThreadControl/IDLocks.cs
+++ Multithreading_Library/ThreadControl/IDLocks.cs
@@ -2,17 +2,27 @@
 
 namespace Multithreading_Library.ThreadControl
 {
+    /// <summary>
+    /// Represents a collection of lock objects identified by keys of type <typeparamref name="T"/>.
+    /// This class facilitates the creation and management of lock objects for synchronization purposes
+    /// in a concurrent environment.
+    /// </summary>
+    /// <typeparam name="T">The type of the keys used to identify lock objects.</typeparam>
     public class IDLocks<T>
     {
-        ConcurrentDictionary<T,object> locks = new ConcurrentDictionary<T,object>();
+        private ConcurrentDictionary<T, object> locks = new ConcurrentDictionary<T, object>();
+
         /// <summary>
-        /// obtain (get or create) a lock object with the specific id)
+        /// Obtain (get or create) a lock object associated with a specific id. If a lock object with the specified
+        /// id doesn't already exist, a new one is created. This method is useful for synchronizing access
+        /// to resources that are identified by keys.
         /// </summary>
-        /// <param name="key"></param>
-        /// <returns></returns>
+        /// <param name="key">The key to identify the lock object.</param>
+        /// <returns>An object that can be used as a lock in synchronization scenarios.</returns>
         public object ObtainLockObject(T key)
         {
-            return locks.GetOrAdd(key, new object());
+            return locks.GetOrAdd(key, _ => new object());
         }
     }
+
 }
